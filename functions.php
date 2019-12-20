2d7b55b8aea18221503fc1da3dd076e8b05b6a5c
Wildiney Di Masi <827978+wildiney@users.noreply.github.com>
2019-12-20 20:46:26 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,4 @@
 <?php
-update_option('siteurl', 'http://localhost:8300');
-update_option('home', 'http://localhost:8300');
-
 add_filter('show_admin_bar', '__return_false');
 add_theme_support('automatic-feed-links');
 add_theme_support('post-thumbnails');
