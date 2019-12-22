ad13bcfdae5e353792e6ea3d5d3e4659e919b99f
Wildiney Di Masi <wildiney@gmail.com>
2019-12-22 19:14:27 -0300

    updating header

--- single.php
+++ single.php
@@ -1,16 +1,18 @@
 <?php get_header(); ?>
-<div class="container">
-  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-      <?php the_title("<h2>", "</h2>"); ?>
-      <header id="single-post">
-        <?php if (has_post_thumbnail()) {
-          the_post_thumbnail('large');
-        } ?>
-      </header>
-      <p><?php the_tags() ?><p>
-          <?php the_content(); ?>
-      <?php endwhile;
-  endif;
-      ?>
-</div>
+<?php get_template_part('template-parts/single-header'); ?>
+
+<main>
+  <section id="single">
+    <div class="container">
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+          <?php the_title("<h2>", "</h2>"); ?>
+          
+          <p><?php the_tags() ?><p>
+              <?php the_content(); ?>
+          <?php endwhile;
+      endif;
+          ?>
+    </div>
+  </section>
+</main>
 <?php get_footer(); ?>
