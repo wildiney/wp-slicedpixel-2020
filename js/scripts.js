50328aa51ecee9b92d879d8b416d44e2c4e7c7f3
Fernando Di Masi <wildiney@gmail.com>
2020-01-09 17:40:44 -0300

    fixing footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 const menu_btn = document.querySelector(".menu__mobile");
 const menu = document.querySelector(".header__menu");
 const arrow = document.querySelector(".header__scroll--arrow");
+const backdrop = document.querySelector(".header__menu");
 
 menu_btn.addEventListener('click', () => {
+  menu.classList.toggle('showhide');
+  arrow.classList.toggle('hide');
+});
+
+backdrop.addEventListener('click', () => {
   menu.classList.toggle('showhide');
   arrow.classList.toggle('hide');
 });
