26330507a15e0ba35e3ea2ef721c2584716b1847
Wildiney Di Masi <wildiney@gmail.com>
2020-01-09 22:52:11 -0300

    code verification

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="google-site-verification" content="xrI_4mcc00pDOswbkBdj-gnbPYx8kHelqfFxycNWD34" />
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
   <?php wp_head(); ?>
