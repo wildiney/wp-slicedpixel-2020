4304c1b7d8dcf4172099fa5ad6db59faf17484ad
Wildiney Di Masi <wildiney@gmail.com>
2020-01-09 23:51:11 -0300

    category page added

--- functions.php
+++ functions.php
@@ -59,3 +59,16 @@ function custom_post_types()
   ));
 }
 add_action('init', 'custom_post_types');
+
+function query_post_type($query) {
+  if( is_category() ) {
+    $post_type = get_query_var('post_type');
+    if($post_type)
+        $post_type = $post_type;
+    else
+        $post_type = array('works'); // don't forget nav_menu_item to allow menus to work!
+    $query->set('post_type',$post_type);
+    return $query;
+    }
+}
+add_filter('pre_get_posts', 'query_post_type');
